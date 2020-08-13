a2c49156bde6f71a21d65839cb649a57606244d6
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-13 01:31:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ while outer_index < src.length do
   inner_index = 0 
   
   while inner_index < src[outer_index].length do 
-    if src[outer_index][inner_index].kind_of?(String) == true do 
+    if src[outer_index][inner_index].kind_of?(String) === true do 
       new_string_array << src[outer_index][inner_index]
     end 
     inner_index += 1 
