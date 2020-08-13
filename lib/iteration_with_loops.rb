edd3f42cd59a7185589e48f012f503c2ffa508ce
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-13 01:14:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,9 @@ while outer_index < src.length do
   inner_index = 0 
   
   while inner_index < src[outer_index].length do 
-    if src[outer_index][inner_index].class = 
+    if src[outer_index][inner_index].class = String do 
+      new_string_array << src[outer_index][inner_index]
+    end 
     inner_index += 1 
   end
     outer_index += 1 
